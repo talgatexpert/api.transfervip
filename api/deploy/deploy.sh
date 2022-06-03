19d558de29277f45f2f1c97903627650b810d44b
talgatexpert <49163220+talgatexpert@users.noreply.github.com>
2022-06-03 13:57:30 +0300

    update

--- api/deploy/deploy.sh
+++ api/deploy/deploy.sh
@@ -5,25 +5,25 @@ cd /home/lux/lux_elit
 git pull
 
 
-cd api
-# update PHP dependencies
-
-php artisan down
-
-
-composer install --no-interaction --no-dev --prefer-dist
-# --no-interaction Do not ask any interactive question
-# --no-dev  Disables installation of require-dev packages.
-# --prefer-dist  Forces installation from package dist even for dev versions.
-# update database
-php artisan migrate --force
-# --force  Required to run when in production.
-# stop maintenance mode
-php artisan up
-php artisan optimize
+#cd api
+## update PHP dependencies
+#
+#php artisan down
+#
+#
+#composer install --no-interaction --no-dev --prefer-dist
+## --no-interaction Do not ask any interactive question
+## --no-dev  Disables installation of require-dev packages.
+## --prefer-dist  Forces installation from package dist even for dev versions.
+## update database
+#php artisan migrate --force
+## --force  Required to run when in production.
+## stop maintenance mode
+#php artisan up
+#php artisan optimize
 
 cd ../client
 
 pm2 kill
 npm run build
-pm2 start
+pm2 start /home/lux/lux_elit/client/ecosystem.config.js
