35e8c719fe1b728d91037b96804dacd9f5c0483e
talgatexpert <49163220+talgatexpert@users.noreply.github.com>
2022-06-03 14:29:53 +0300

    update

--- api/deploy/deploy.sh
+++ api/deploy/deploy.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 # activate maintenance mode
+# shellcheck disable=SC2164
 cd /home/lux/lux_elit
 # update source code
 git pull
 
 
-#cd api
-## update PHP dependencies
-#
-#php artisan down
-#
-#
-#composer install --no-interaction --no-dev --prefer-dist
-## --no-interaction Do not ask any interactive question
-## --no-dev  Disables installation of require-dev packages.
-## --prefer-dist  Forces installation from package dist even for dev versions.
-## update database
-#php artisan migrate --force
-## --force  Required to run when in production.
-## stop maintenance mode
-#php artisan up
-#php artisan optimize
+# shellcheck disable=SC2164
+cd /home/lux/lux_elit/api
+# update PHP dependencies
+
+php artisan down
+
+
+composer install --no-interaction --no-dev --prefer-dist
+# --no-interaction Do not ask any interactive question
+# --no-dev  Disables installation of require-dev packages.
+# --prefer-dist  Forces installation from package dist even for dev versions.
+# update database
+php artisan migrate --force
+# --force  Required to run when in production.
+# stop maintenance mode
+php artisan up
+php artisan optimize
 
 cd /home/lux/lux_elit/client/
 
